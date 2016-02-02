cf7f2315eb068692950bf348829b4a61d623a46f
thomasjacobjr <thomasjacobjr@gmail.com>
2016-02-01 18:17:14 -0700

    Assignment 30 - Rails - Seeding Data : Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,10 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "A Unique Title", body: "A unique body")
+puts "#{Post.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"

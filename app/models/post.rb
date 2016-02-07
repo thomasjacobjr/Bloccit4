82898c1687ca3783656f5d654bdd035e54c0f40e
thomasjacobjr <thomasjacobjr@gmail.com>
2016-02-07 16:17:10 -0700

    Assignment 44 - Rails - Favorites - Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
-  has_many :favorites, dependent: :destroy 
+  has_many :favorites, dependent: :destroy
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
+  after_create :create_favorite
 
   default_scope { order('rank DESC') }
 
@@ -31,4 +32,9 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
   end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
+  end 
 end

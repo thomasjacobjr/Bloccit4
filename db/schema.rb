25de11a0e95d061a5f6b038022c78dc49ecae7bb
thomasjacobjr <thomasjacobjr@gmail.com>
2016-02-07 12:19:45 -0700

    Checkpoint 40 - Rails - Authorization : Completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207175206) do
+ActiveRecord::Schema.define(version: 20160207185622) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160207175206) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -36,10 +36,19 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'thomasjacobjr@gmail.com',
-  password: 'helloworld'
+# Create an Admin User
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+# Create a member
+member = User.create!(
+name: 'Member User',
+email: 'member@example.com',
+password: 'helloworld'
 )
 
 puts "Seed finished"

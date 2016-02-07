cbc466d569d8ea3009a3a965c0f4dfb607a601c1
thomasjacobjr <thomasjacobjr@gmail.com>
2016-02-07 11:18:36 -0700

    Checkpoint 39 - Rails - Posts and Users : Completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-   # Shoulda tests for name
+
+  it { is_expected.to have_many(:posts) }
+  
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-   # Shoulda tests for email
+
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
   it { should_not allow_value("userbloccit.com").for(:email) }
 
-   # Shoulda tests for password
+
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
@@ -44,5 +46,5 @@ RSpec.describe User, type: :model do
     it "should be an invalid user due to incorrectly formatted email address" do
       expect(user_with_invalid_email_format).to_not be_valid
     end
-  end  
+  end
 end

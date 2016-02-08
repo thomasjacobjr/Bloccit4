196c5c38460055c483edd0e5bc41fe5ba1403e76
thomasjacobjr <thomasjacobjr@gmail.com>
2016-02-08 10:14:53 -0700

    Assignment 47 - Rails - Ajax : Completed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,12 +3,20 @@ class VotesController < ApplicationController
 
   def up_vote
     update_vote(1)
-    redirect_to :back
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def down_vote
     update_vote(-1)
-    redirect_to :back
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
@@ -21,5 +29,5 @@ class VotesController < ApplicationController
     else
       @vote = current_user.votes.create(value: new_value, post: @post)
     end
-  end  
+  end
 end
